9131b180d36eb3d54d2934df007bb5ea72ac634d
josue1290 <ti1601001@academica.utbb.edu.mx>
2022-10-18 19:59:24 -0500

    correcciones

--- directorio.js
+++ directorio.js
@@ -21,9 +21,9 @@ function pintar(){
     if(amigos.length>0)
     {
         lista.innerHTML="";
-        amigos.forEach((contacto)=>{
+        amigos.forEach((contacto,index)=>{
             let amigo=document.createElement("div");
-            amigo.innerHTML=`<p>${contacto.nombre}</p><button class="muestraDetalles"><input type="hidden" value="${contacto.telefono}"/>Detalles</button>`;
+            amigo.innerHTML=`<p>${contacto.nombre}</p><button class="muestraDetalles"><input type="hidden" value="${contacto.telefono}"/>Detalles</button><button class="eliminarContacto" indice="${index}" onClick="return confirm('¿Estás seguro de eliminar a "${contacto.nombre}"?">Borrar</button>`;
             lista.appendChild(amigo);
         });
         let botones=document.getElementsByClassName("muestraDetalles");
@@ -33,6 +33,15 @@ function pintar(){
                 showDetalles(element.children[0].value); 
             });
         }
+        botones=document.getElementsByClassName("eliminarContacto");
+        for (let i = 0; i < botones.length; i++) {
+            const element = botones[i];
+            element.addEventListener("click",()=>{
+                amigos.splice(element.getAttribute("indice"),1);
+                pintar(); 
+            });
+        }
+
     }
     else{
         lista.innerHTML="<h2>No tenemos amigos</h2>";
@@ -71,17 +80,26 @@ btnCancelar.addEventListener("click",(event)=>{
 })
 
 
-btnGuardar.addEventListener("click",(event)=>{
-    let contacto={
-        nombre:formulario["nombre"].value,
-        telefono:formulario["telefono"].value,
-        correo:formulario["correo"].value,
-        foto:formulario["foto"].value
-    };
+btnGuardar.addEventListener("click",
 
-    amigos.push(contacto);
-    limpiar();
-    pintar();
-    event.preventDefault();
-    
+(event)=>{
+
+    if(formulario[0].value!="" && formulario[1].value!="" && formulario[2].value!="" && formulario[3].value!=""){
+        let contacto={
+            nombre:formulario["nombre"].value,
+            telefono:formulario["telefono"].value,
+            correo:formulario["correo"].value,
+            foto:formulario["foto"].value
+        };
+
+        amigos.push(contacto);
+        limpiar();
+        pintar();
+        event.preventDefault();
+    }
+    else{
+
+        event.preventDefault();
+        swal("Oops!", "Faltaron campos por rellenar", "error");
+    }  
 })
